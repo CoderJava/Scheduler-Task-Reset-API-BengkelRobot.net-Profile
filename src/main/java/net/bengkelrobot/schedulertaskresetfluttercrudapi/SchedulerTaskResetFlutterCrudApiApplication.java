275fe2c5193d20556bcc1f2f74779551cf4c8b00
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-01-19 18:48:09 +0700

    Added index controller
    Added index controller for make sure that this server is running

--- src/main/java/net/bengkelrobot/schedulertaskresetfluttercrudapi/SchedulerTaskResetFlutterCrudApiApplication.java
+++ src/main/java/net/bengkelrobot/schedulertaskresetfluttercrudapi/SchedulerTaskResetFlutterCrudApiApplication.java
@@ -3,13 +3,21 @@ package net.bengkelrobot.schedulertaskresetfluttercrudapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
+@RestController
 public class SchedulerTaskResetFlutterCrudApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchedulerTaskResetFlutterCrudApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerTaskResetFlutterCrudApiApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "Server is running";
+    }
 
 }
